65b71532860bdd81bb9e112665daccdcf9ca62bc
Shivangam Soni <shivangamsoni99@gmail.com>
2023-02-16 22:47:02 +0530

    Add: Filter Functionality

--- src/components/rent/Filter.tsx
+++ src/components/rent/Filter.tsx
@@ -1,10 +1,126 @@
 import { useState, useMemo } from "react";
 
+import { useProperty } from "@context/PropertyContext";
 import { getList } from "@lib/getList";
-import SampleData from "@data/sample.json";
 
 import ListMenu from "@components/common/ListMenu";
 import Button from "@components/common/Button";
+
+export default function Filter() {
+    const { properties, setFilters, resetFilters } = useProperty();
+
+    // Locations
+    const locations = useMemo(() => {
+        return getList(
+            properties,
+            ({ address: { state, country } }) => {
+                return `${state}, ${country}`;
+            },
+            "All",
+        );
+    }, []);
+    const [selectedLocationId, setSelectedLocationId] = useState(
+        locations[0].id,
+    );
+
+    // Bedrooms
+    const bedrooms = useMemo(() => {
+        return getList(properties, ({ bedrooms }) => bedrooms, "All");
+    }, []);
+    const [selectedBedroomId, setSelectedBedroomId] = useState(bedrooms[0].id);
+
+    // Price Range
+    const [selectedPriceId, setSelectedPriceId] = useState(PriceRanges[0].id);
+
+    // Property Types
+    const propertyTypes = useMemo(() => {
+        return getList(properties, ({ type }) => type, "All");
+    }, []);
+    const [selectedTypeId, setSelectedTypeId] = useState(propertyTypes[0].id);
+
+    function handleReset() {
+        setSelectedLocationId(locations[0].id);
+        setSelectedBedroomId(bedrooms[0].id);
+        setSelectedPriceId(PriceRanges[0].id);
+        setSelectedTypeId(propertyTypes[0].id);
+        resetFilters();
+    }
+
+    function handleSearch() {
+        const location = locations.find(
+            ({ id }) => id === selectedLocationId,
+        )!.label;
+
+        const bedroom = bedrooms.find(
+            ({ id }) => id === selectedBedroomId,
+        )!.label;
+
+        const priceRange = PriceRanges.find(
+            ({ id }) => id === selectedPriceId,
+        )!;
+
+        const propertyType = propertyTypes.find(
+            ({ id }) => id === selectedTypeId,
+        )!.label;
+
+        // TODO: Filter properties based data
+        console.log({ location, bedroom, priceRange, propertyType });
+        setFilters({ location, bedroom, priceRange, propertyType });
+    }
+
+    return (
+        <div className="bg-white p-6 rounded-md shadow-lg grid grid-cols-5 divide-x">
+            <div className="flex flex-col justify-center px-4">
+                <h2 className="text-slate-400 font-semibold text-sm">
+                    Location
+                </h2>
+                <ListMenu
+                    items={locations}
+                    selectedId={selectedLocationId}
+                    onChange={(id) => setSelectedLocationId(id)}
+                />
+            </div>
+
+            <div className="flex flex-col px-4 justify-center">
+                <h2 className="text-slate-400 font-semibold text-sm">
+                    Bedrooms
+                </h2>
+                <ListMenu
+                    items={bedrooms}
+                    selectedId={selectedBedroomId}
+                    onChange={(id) => setSelectedBedroomId(id)}
+                />
+            </div>
+
+            <div className="flex flex-col px-4 justify-center">
+                <h2 className="text-slate-400 font-semibold text-sm">Price</h2>
+                <ListMenu
+                    items={PriceRanges}
+                    selectedId={selectedPriceId}
+                    onChange={(id) => setSelectedPriceId(id)}
+                />
+            </div>
+
+            <div className="flex flex-col px-4 justify-center">
+                <h2 className="text-slate-400 font-semibold text-sm">
+                    Property Type
+                </h2>
+                <ListMenu
+                    items={propertyTypes}
+                    selectedId={selectedTypeId}
+                    onChange={(id) => setSelectedTypeId(id)}
+                />
+            </div>
+
+            <div className="flex flex-col px-4 justify-center gap-2">
+                <Button onClick={handleSearch}>Search</Button>
+                <Button variant="outlined" onClick={handleReset}>
+                    Reset
+                </Button>
+            </div>
+        </div>
+    );
+}
 
 const PriceRanges = [
     {
@@ -39,102 +155,4 @@ const PriceRanges = [
     },
 ];
 
-export default function Filter() {
-    // Locations
-    const locations = useMemo(() => {
-        return getList(
-            SampleData,
-            ({ address: { state, country } }) => {
-                return `${state}, ${country}`;
-            },
-            "All",
-        );
-    }, []);
-    const [selectedLocationId, setSelectedLocationId] = useState(
-        locations[0].id,
-    );
-
-    // Bedrooms
-    const bedrooms = useMemo(() => {
-        return getList(SampleData, ({ bedrooms }) => bedrooms, "All");
-    }, []);
-    const [selectedBedroomId, setSelectedBedroomId] = useState(bedrooms[0].id);
-
-    // Price Range
-    const [selectedPriceId, setSelectedPriceId] = useState(PriceRanges[0].id);
-
-    // Property Types
-    const propertyTypes = useMemo(() => {
-        return getList(SampleData, ({ type }) => type, "All");
-    }, []);
-    const [selectedTypeId, setSelectedTypeId] = useState(propertyTypes[0].id);
-
-    function handleSearch() {
-        const location = locations.find(
-            ({ id }) => id === selectedLocationId,
-        )!.label;
-
-        const bedroom = bedrooms.find(
-            ({ id }) => id === selectedBedroomId,
-        )!.label;
-
-        const priceRange = PriceRanges.find(({ id }) => id === selectedPriceId);
-
-        const propertyType = propertyTypes.find(
-            ({ id }) => id === selectedTypeId,
-        )!.label;
-
-        // TODO: Filter properties based data
-        console.log({ location, bedroom, priceRange, propertyType });
-    }
-
-    return (
-        <div className="bg-white p-6 rounded-md shadow-lg grid grid-cols-5 divide-x">
-            <div className="flex flex-col px-4">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Location
-                </h2>
-                <ListMenu
-                    items={locations}
-                    selectedId={selectedLocationId}
-                    onChange={(id) => setSelectedLocationId(id)}
-                />
-            </div>
-
-            <div className="flex flex-col px-4">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Bedrooms
-                </h2>
-                <ListMenu
-                    items={bedrooms}
-                    selectedId={selectedBedroomId}
-                    onChange={(id) => setSelectedBedroomId(id)}
-                />
-            </div>
-
-            <div className="flex flex-col px-4">
-                <h2 className="text-slate-400 font-semibold text-sm">Price</h2>
-                <ListMenu
-                    items={PriceRanges}
-                    selectedId={selectedPriceId}
-                    onChange={(id) => setSelectedPriceId(id)}
-                />
-            </div>
-
-            <div className="flex flex-col px-4">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Property Type
-                </h2>
-                <ListMenu
-                    items={propertyTypes}
-                    selectedId={selectedTypeId}
-                    onChange={(id) => setSelectedTypeId(id)}
-                />
-            </div>
-
-            <div className="flex flex-col px-4 justify-center ">
-                <Button onClick={handleSearch}>Search</Button>
-            </div>
-        </div>
-    );
-}
+export type IPriceRange = typeof PriceRanges[number];
--- src/pages/Rent.tsx
+++ src/pages/Rent.tsx
@@ -2,31 +2,39 @@ import Filter from "@components/rent/Filter";
 import PropertyCard from "@components/rent/PropertyCard";
 import Search from "@components/rent/Search";
 
-import SampleData from "@data/sample.json";
+import { useProperty } from "@context/PropertyContext";
 
 export default function Rent() {
+    const { filteredProperties } = useProperty();
+
     return (
         <section className="grid gap-12 py-4">
             <Search />
             <Filter />
 
-            <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-                {SampleData.map((property) => (
-                    <li key={property.id}>
-                        <PropertyCard
-                            id={property.id}
-                            type={property.type}
-                            image={property.image}
-                            name={property.name}
-                            price={property.price}
-                            address={property.address.full}
-                            bedrooms={property.bedrooms}
-                            bathrooms={property.bathrooms}
-                            area={property.area}
-                        />
-                    </li>
-                ))}
-            </ul>
+            {filteredProperties.length === 0 ? (
+                <p className="text-lg font-bold text-center text-violet-800">
+                    No Properties Fit the Applied Filters
+                </p>
+            ) : (
+                <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                    {filteredProperties.map((property) => (
+                        <li key={property.id}>
+                            <PropertyCard
+                                id={property.id}
+                                type={property.type}
+                                image={property.image}
+                                name={property.name}
+                                price={property.price}
+                                address={property.address.full}
+                                bedrooms={property.bedrooms}
+                                bathrooms={property.bathrooms}
+                                area={property.area}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            )}
         </section>
     );
 }
