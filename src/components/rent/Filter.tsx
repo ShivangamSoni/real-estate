4ab6c77e9d093fca4b022ea7f653dfd26e073f8e
Shivangam Soni <shivangamsoni99@gmail.com>
2023-02-17 00:33:19 +0530

    Update: Responsive Filter Bar Styling

--- src/components/rent/Filter.tsx
+++ src/components/rent/Filter.tsx
@@ -3,11 +3,14 @@ import { useState, useMemo } from "react";
 import { useProperty } from "@context/PropertyContext";
 import { getList } from "@lib/getList";
 
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
+
 import ListMenu from "@components/common/ListMenu";
 import Button from "@components/common/Button";
 
 export default function Filter() {
     const { properties, setFilters, resetFilters } = useProperty();
+    const [open, setOpen] = useState(false);
 
     // Locations
     const locations = useMemo(() => {
@@ -69,54 +72,83 @@ export default function Filter() {
     }
 
     return (
-        <div className="bg-white p-6 rounded-md shadow-lg grid grid-cols-5 divide-x">
-            <div className="flex flex-col justify-center px-4">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Location
-                </h2>
-                <ListMenu
-                    items={locations}
-                    selectedId={selectedLocationId}
-                    onChange={(id) => setSelectedLocationId(id)}
-                />
-            </div>
+        <div className="relative">
+            <Button
+                variant="outlined"
+                className="md:hidden flex items-center justify-center gap-3"
+                aria-controls="filter-bar"
+                aria-expanded={open}
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                <span className="sr-only">Open Filters Menu</span>
+                <span className="">Filters</span>
+                {open ? (
+                    <XMarkIcon className="w-5 h-5" />
+                ) : (
+                    <Bars3Icon className="w-5 h-5" />
+                )}
+            </Button>
+            <div
+                id="filter-bar"
+                className={`absolute top-full left-0 mt-4 z-10 w-full bg-white p-6 rounded-md shadow-lg grid transition-all origin-top-left
+                    ${
+                        open
+                            ? "scale-100 opacity-100"
+                            : "scale-75 opacity-0 pointer-events-none"
+                    }
+                    md:static md:grid-cols-5 md:divide-x md:transition-none md:scale-100 md:opacity-100 md:pointer-events-auto md:mt-0
+                `}
+            >
+                <div className="flex flex-col justify-center px-4">
+                    <h2 className="text-slate-400 font-semibold text-sm">
+                        Location
+                    </h2>
+                    <ListMenu
+                        items={locations}
+                        selectedId={selectedLocationId}
+                        onChange={(id) => setSelectedLocationId(id)}
+                    />
+                </div>
 
-            <div className="flex flex-col px-4 justify-center">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Bedrooms
-                </h2>
-                <ListMenu
-                    items={bedrooms}
-                    selectedId={selectedBedroomId}
-                    onChange={(id) => setSelectedBedroomId(id)}
-                />
-            </div>
+                <div className="flex flex-col px-4 justify-center">
+                    <h2 className="text-slate-400 font-semibold text-sm">
+                        Bedrooms
+                    </h2>
+                    <ListMenu
+                        items={bedrooms}
+                        selectedId={selectedBedroomId}
+                        onChange={(id) => setSelectedBedroomId(id)}
+                    />
+                </div>
 
-            <div className="flex flex-col px-4 justify-center">
-                <h2 className="text-slate-400 font-semibold text-sm">Price</h2>
-                <ListMenu
-                    items={PriceRanges}
-                    selectedId={selectedPriceId}
-                    onChange={(id) => setSelectedPriceId(id)}
-                />
-            </div>
+                <div className="flex flex-col px-4 justify-center">
+                    <h2 className="text-slate-400 font-semibold text-sm">
+                        Price
+                    </h2>
+                    <ListMenu
+                        items={PriceRanges}
+                        selectedId={selectedPriceId}
+                        onChange={(id) => setSelectedPriceId(id)}
+                    />
+                </div>
 
-            <div className="flex flex-col px-4 justify-center">
-                <h2 className="text-slate-400 font-semibold text-sm">
-                    Property Type
-                </h2>
-                <ListMenu
-                    items={propertyTypes}
-                    selectedId={selectedTypeId}
-                    onChange={(id) => setSelectedTypeId(id)}
-                />
-            </div>
+                <div className="flex flex-col px-4 justify-center">
+                    <h2 className="text-slate-400 font-semibold text-sm">
+                        Property Type
+                    </h2>
+                    <ListMenu
+                        items={propertyTypes}
+                        selectedId={selectedTypeId}
+                        onChange={(id) => setSelectedTypeId(id)}
+                    />
+                </div>
 
-            <div className="flex flex-col px-4 justify-center gap-2">
-                <Button onClick={handleSearch}>Search</Button>
-                <Button variant="outlined" onClick={handleReset}>
-                    Reset
-                </Button>
+                <div className="flex flex-col px-4 justify-center gap-2">
+                    <Button onClick={handleSearch}>Search</Button>
+                    <Button variant="outlined" onClick={handleReset}>
+                        Reset
+                    </Button>
+                </div>
             </div>
         </div>
     );
